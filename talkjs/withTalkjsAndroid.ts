749e994769ea39b5be48da7e38ea9ec70eb740dc
Fernand Lime <fernand.lime@gmail.com>
2022-05-30 02:07:17 +0200

    add withTalkjsAndroid.ts

--- talkjs/withTalkjsAndroid.ts
+++ talkjs/withTalkjsAndroid.ts
@@ -1,33 +1,40 @@
-import { ConfigPlugin, withAppBuildGradle } from '@expo/config-plugins'
+import { ConfigPlugin, withProjectBuildGradle } from '@expo/config-plugins'
 
-import { WonderPushPluginProps } from '../types/types'
+// import { TalkjsPluginProps } from '../types/types'
 
-function addWonderPushDefaultConfigBlock(buildGradle: string, props: WonderPushPluginProps) {
-  const pattern = /wonderpush-plugin-default-config/g
+function addTalkjsDefaultConfigBlock(
+  buildGradle: string
+  // props: TalkjsPluginProps
+) {
+  const pattern = /talkjs-plugin-default-config/g
   if (buildGradle.match(pattern)) {
     return buildGradle
   }
 
   return buildGradle.replace(
-    /defaultConfig\s?{/,
-    `defaultConfig {
-        // wonderpush-plugin-default-config
-
-        // Note that it's important to keep the double quotes as part of the third argument
-        // as this represents a string in Java code
-        buildConfigField 'String', 'WONDERPUSH_CLIENT_ID', '"` + props.wonderPushClientId + `"'
-        buildConfigField 'String', 'WONDERPUSH_CLIENT_SECRET', '"` + props.wonderPushClientSecret + `"'
-        buildConfigField 'String', 'WONDERPUSH_SENDER_ID', '"`+ props.wonderPushSenderId +`"'`,
+    /allprojects\s{\r\n\s{4}repositories\s{/,
+    `allprojects {
+    repositories {
+        // talkjs-plugin-default-config
+        maven {
+          url "$rootDir/../node_modules/@notifee/react-native/android/libs"
+        }`
   )
 }
 
-export const withWonderPushAndroid: ConfigPlugin<WonderPushPluginProps> = (config, props) => {
-  return withAppBuildGradle(config, (config) => {
+export const withTalkjsAndroid: ConfigPlugin<any> = (
+  config
+  // props
+) => {
+  return withProjectBuildGradle(config, (config) => {
     if (config.modResults.language === "groovy") {
-      config.modResults.contents = addWonderPushDefaultConfigBlock(config.modResults.contents, props)
+      config.modResults.contents = addTalkjsDefaultConfigBlock(
+        config.modResults.contents
+        // props
+      )
     } else {
       throw new Error(
-        "Cannot add WonderPush maven gradle because the project build.gradle is not groovy",
+        "Cannot add TalkJS maven gradle because the project build.gradle is not groovy"
       )
     }
     return config
