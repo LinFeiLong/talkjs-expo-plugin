6697436195e9d7bba939732df61dea19480287f6
Fernand Lime <fernand.lime@gmail.com>
2022-05-30 04:44:25 +0200

    change name

--- talkjs/withTalkjs.ts
+++ talkjs/withTalkjs.ts
@@ -5,16 +5,15 @@
 
 import { ConfigPlugin } from '@expo/config-plugins'
 
-import { WonderPushPluginProps } from '../types/types'
 import { withTalkjsAndroid } from './withTalkjsAndroid'
 
 // import { withWonderPushIos } from './withTalkjsIos'
 
-const withWonderPush: ConfigPlugin<WonderPushPluginProps> = (config) => {
+const withTalkjs: ConfigPlugin = (config) => {
   // config = withWonderPushIos(config, props)
   config = withTalkjsAndroid(config)
 
   return config
 }
 
-export default withWonderPush
+export default withTalkjs
